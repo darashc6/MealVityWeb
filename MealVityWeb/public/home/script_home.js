87fc7380d07d24666754284e823d0b78a33a8c8b
Darashc <darashc6@gmail.com>
2020-09-16 18:55:52 +0200

    Address autocomplete in 'Home' page
    - Inserting your address now gives you a dropdown list of suggestions for quicker completion.
    - Bug: Fixed inconsistent scrollbar appearance in the 'Accounts' page.

--- MealVityWeb/public/home/script_home.js
+++ MealVityWeb/public/home/script_home.js
@@ -1,13 +1,60 @@
-const inputAddress = document.querySelector('.input-address')
-const btnSearch = document.querySelector('#btn-search')
+const inputAddress = document.querySelector('.input-address');
+const btnSearch = document.querySelector('#btn-search');
+const autocompleteResultsDiv = document.querySelector('.autocomplete-results');
+let addressSelected = false;
+
+inputAddress.addEventListener('input', async () => {
+    if (inputAddress.value.length !== 0) {
+        if (addressSelected && inputAddress.value !== sessionStorage.getItem('address')) {
+            addressSelected = false;
+        }
+
+        await fetchAutocomplete();
+
+        const results = document.querySelectorAll('.result');
+        results.forEach(result => {
+            result.addEventListener('click', () => {
+                inputAddress.value = result.textContent;
+                addressSelected = true;
+                sessionStorage.setItem("address", inputAddress.value)
+                autocompleteResultsDiv.innerHTML = '';
+            });
+        });
+
+    } else {
+        autocompleteResultsDiv.innerHTML = '';
+    }
+});
 
 btnSearch.addEventListener('click', () => {
     if (inputAddress.value.length !== 0) {
-        sessionStorage.setItem("address", inputAddress.value)
-        console.log(sessionStorage.getItem("address"))
-        location.href = "search_results/search_results.html"
+        if (addressSelected) {
+            location.href = "search_results/search_results.html"
+        } else {
+            alert('Please select a valid address')
+            inputAddress.focus();
+        }
+        console.log(addressSelected)
     } else {
-        alert('Please enter a valid home address')
+        alert('Please enter a valid address')
         inputAddress.focus();
     }
-})
+});
+
+async function fetchAutocomplete() {
+    const response = await fetch(`/autocomplete/${inputAddress.value}`);
+    const autocomplete_json = await response.json();
+    // console.log(autocomplete_json)
+
+    autocompleteResultsDiv.innerHTML = '';
+    autocomplete_json.items.map(result => {
+        createAutocompleteResult(result.title)
+    });
+}
+
+function createAutocompleteResult(address) {
+    const resultDiv = document.createElement('div');
+    resultDiv.classList.add('result');
+    resultDiv.textContent = address;
+    autocompleteResultsDiv.appendChild(resultDiv);
+}
--- MealVityWeb/server.js
+++ MealVityWeb/server.js
@@ -8,14 +8,11 @@ app.listen(process.env.PORT || 3000, () => {});
 app.use(express.static('public'));
 app.use(express.json());
 
-const yelp_api_search_url = new URL(process.env.YELP_API_SEARCH_URL)
-const categoryType = 'restaurant';
-const searchLimit = 50;
-
+const yelp_api_search_url = new URL(process.env.YELP_API_SEARCH_URL);
 app.get('/business_search/:paramsString', async (req, res) => {
     const paramsJson = JSON.parse(req.params.paramsString);
-    paramsJson.categories = categoryType;
-    paramsJson.limit = searchLimit;
+    paramsJson.categories = 'restaurant';
+    paramsJson.limit = 50;
     
     yelp_api_search_url.search = new URLSearchParams(paramsJson).toString();
 
@@ -27,4 +24,24 @@ app.get('/business_search/:paramsString', async (req, res) => {
     const yelp_json = await yelp_response.json();
 
     res.json(yelp_json);
-})
+});
+
+const here_api_autocomplete_url = new URL(process.env.HERE_API_AUTOCOMPLETE_URL)
+const here_api_key = process.env.HERE_API_KEY
+app.get('/autocomplete/:address', async (req, res) => {
+    const params = {
+        q: req.params.address,
+        at: '40.4637,3.7492',
+        in: 'countryCode:ESP',
+        resultTypes: 'street',
+        limit: 5,
+        apiKey: here_api_key
+    }
+
+    here_api_autocomplete_url.search = new URLSearchParams(params)
+    
+    const response = await fetch(here_api_autocomplete_url.href);
+    const response_json = await response.json();
+
+    res.json(response_json);
+});
