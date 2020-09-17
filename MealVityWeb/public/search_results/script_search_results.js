e96388072607bbc48d4c84889a1e3f24cec45ab1
Darashc <darashc6@gmail.com>
2020-09-17 20:50:15 +0200

    Search results redesigned
    - Redesigned the return list
    - Added button to redirect to the restaurant's Yelp website
    - Added button to redirect to the restaurant's location in Google Maps

--- MealVityWeb/public/search_results/script_search_results.js
+++ MealVityWeb/public/search_results/script_search_results.js
@@ -50,7 +50,9 @@ function createContainer(restaurantData) {
     restaurantImage.src = restaurantData.image_url;
     const restaurantContent = document.createElement('div');
     restaurantContent.classList.add('restaurant-content');
-    resultContainer.append(restaurantImage, restaurantContent);
+    const externalLinksDiv = document.createElement('div');
+    externalLinksDiv.classList.add('external-links');
+    resultContainer.append(restaurantImage, restaurantContent, externalLinksDiv);
 
     const restaurantNameDiv = document.createElement('label');
     restaurantNameDiv.classList.add('restaurant-name');
@@ -59,29 +61,13 @@ function createContainer(restaurantData) {
     restaurantDetailsDiv.classList.add('restaurant-details');
     restaurantContent.append(restaurantNameDiv, restaurantDetailsDiv);
 
-    const restaurantInfoDiv = document.createElement('div');
-    const restaurantInfoDiv2 = document.createElement('div');
-    const restaurantInfoDiv3 = document.createElement('div');
-    restaurantInfoDiv.classList.add('restaurant-info');
-    restaurantInfoDiv2.classList.add('restaurant-info');
-    restaurantInfoDiv3.classList.add('restaurant-info');
-    restaurantDetailsDiv.append(restaurantInfoDiv, restaurantInfoDiv2, restaurantInfoDiv3);
-
     const cuisineDiv = document.createElement('div');
     const cuisineImg = document.createElement('img');
     cuisineImg.src = 'images/spoon.png';
     const cuisineLabel = document.createElement('label');
     cuisineLabel.textContent = restaurantData.categories[0].title;
     cuisineDiv.append(cuisineImg, cuisineLabel);
-    restaurantInfoDiv.appendChild(cuisineDiv);
-
-    const phoneDiv = document.createElement('div');
-    const phoneImg = document.createElement('img');
-    phoneImg.src = 'images/phone.png';
-    const phoneLabel = document.createElement('label');
-    phoneLabel.textContent = restaurantData.phone;
-    phoneDiv.append(phoneLabel, phoneImg);
-    restaurantInfoDiv.appendChild(phoneDiv);
+    restaurantDetailsDiv.appendChild(cuisineDiv);
 
     const addressDiv = document.createElement('div');
     const addressImg = document.createElement('img');
@@ -89,15 +75,23 @@ function createContainer(restaurantData) {
     const addressLabel = document.createElement('label');
     addressLabel.textContent = restaurantData.location.address1;
     addressDiv.append(addressImg, addressLabel);
-    restaurantInfoDiv2.appendChild(addressDiv);
+    restaurantDetailsDiv.appendChild(addressDiv);
+
+    const priceDiv = document.createElement('div');
+    const priceImg = document.createElement('img');
+    priceImg.src = 'images/euro.png';
+    const priceLabel = document.createElement('label');
+    priceLabel.textContent = restaurantData.price;
+    priceDiv.append(priceImg, priceLabel);
+    restaurantDetailsDiv.appendChild(priceDiv);
 
     const distanceDiv = document.createElement('div');
     const distanceImg = document.createElement('img');
     distanceImg.src = 'images/distance.png';
     const distanceLabel = document.createElement('label');
     distanceLabel.textContent = `${Math.floor(restaurantData.distance)} m`;
-    distanceDiv.append(distanceLabel, distanceImg);
-    restaurantInfoDiv2.appendChild(distanceDiv);
+    distanceDiv.append(distanceImg, distanceLabel);
+    restaurantDetailsDiv.appendChild(distanceDiv);
 
     const ratingDiv = document.createElement('div');
     const ratingImg = document.createElement('img');
@@ -105,15 +99,27 @@ function createContainer(restaurantData) {
     const ratingLabel = document.createElement('label');
     ratingLabel.textContent = restaurantData.rating;
     ratingDiv.append(ratingImg, ratingLabel);
-    restaurantInfoDiv3.appendChild(ratingDiv)
+    restaurantDetailsDiv.appendChild(ratingDiv);
 
-    const priceDiv = document.createElement('div');
-    const priceImg = document.createElement('img');
-    priceImg.src = 'images/euro.png';
-    const priceLabel = document.createElement('label');
-    priceLabel.textContent = restaurantData.price;
-    priceDiv.append(priceLabel, priceImg);
-    restaurantInfoDiv3.appendChild(priceDiv);
+    const yelpExternalLink = document.createElement('div');
+    yelpExternalLink.classList.add('yelp-external-link');
+    const googleMapsExternalLink = document.createElement('div');
+    googleMapsExternalLink.classList.add('google-maps-external-link');
+    externalLinksDiv.append(yelpExternalLink, googleMapsExternalLink);
+
+    const yelpExternalLinkName = document.createElement('p');
+    yelpExternalLinkName.textContent = 'Visit Yelp';
+    yelpExternalLinkName.classList.add('external-link-name');
+    const yelpImage = document.createElement('img');
+    yelpImage.src = 'images/yelp.svg';
+    yelpExternalLink.append(yelpExternalLinkName, yelpImage)
+
+    const GoogleMapsExternalLinkName = document.createElement('p');
+    GoogleMapsExternalLinkName.textContent = 'Google Maps';
+    GoogleMapsExternalLinkName.classList.add('external-link-name');
+    const GoogleMapsImage = document.createElement('img');
+    GoogleMapsImage.src = 'images/google-maps.svg';
+    googleMapsExternalLink.append(GoogleMapsExternalLinkName, GoogleMapsImage)
 }
 
 async function fetchYelp(extraParams) {
@@ -141,6 +147,8 @@ async function fetchYelp(extraParams) {
     });
 
     filterRestaurantList(prevRadioValue);
+    setupYelpLinks(restaurantsList);
+    setupGoogleMapsLinks(restaurantsList);
 }
 
 function filterParameters() {
@@ -221,6 +229,32 @@ function reorderRestaurantList(restaurantList) {
     restaurantList.map(restaurant => {
         createContainer(restaurant);
     });
+}
+
+function setupYelpLinks(restaurantList) {
+    const yelpExternalLinks = document.querySelectorAll('.yelp-external-link');
+
+    yelpExternalLinks.forEach(link => {
+        link.addEventListener('click', ()=> {
+            window.open(restaurantList[[].indexOf.call(yelpExternalLinks, link)].url, '_blank');
+        });
+    });
+}
+
+
+function setupGoogleMapsLinks(restaurantList) {
+    const googleMapsExternalLinks = document.querySelectorAll('.google-maps-external-link');
+
+    googleMapsExternalLinks.forEach(link => {
+        link.addEventListener('click', ()=> {
+            const index = [].indexOf.call(googleMapsExternalLinks, link);
+            const restaurantName = restaurantList[index].name;
+            const restaurantCoordinates = restaurantsList[index].coordinates;
+
+            window.open(`https://www.google.com/maps/search/${restaurantName}/@${restaurantCoordinates.latitude},${restaurantCoordinates.longitude}`);
+        });
+    })
+
 }
 
 fetchYelp([]);
